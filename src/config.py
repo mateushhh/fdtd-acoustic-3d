3fbf2ee55090e6f35a9a6508e610cca8fdc61c1f
pomiano <miloszpomian@gmail.com>
2026-01-23 00:16:31 +0100

    dzialajacy, lecz do uporzadkowania 2d-neumann

--- src/config.py
+++ src/config.py
@@ -8,6 +8,8 @@ def get_time_step(dimensions, dx, speed, safety_factor):
 C = 343.0   # predkosc [m/s]
 FREQ_MAX = 1000.0 # maksymalna czestotliwpsc fali [Hz]
 SIZE_M = 20.0  # rozmiar [m]
+SIGMA = math.sqrt(2 * math.log(2)) / (2 * math.pi * FREQ_MAX)
+AMPLITUDE = 10.0
 
 NODES_PER_WAVELENGTH = 10    # ilsoc punktow na dlugosc fali
 SAFETY_FACTOR = 0.99 # wspolczynnik bezpieczenstwa
@@ -17,11 +19,22 @@ WAVELENGTH = C / FREQ_MAX    # dlugosc fali [m]
 DX = WAVELENGTH / NODES_PER_WAVELENGTH   # krok odleglosciowy [m]
 
 N = int(SIZE_M / DX)     # ilosc punktow w jednym wymiarze
+N_2 = N + 2 # dodajemy po jednym punckie na kazda strone do obliczen
 
 DT = get_time_step(DIM, DX, C, SAFETY_FACTOR)
 
 COURANT = (1.0 / math.sqrt(DIM)) * SAFETY_FACTOR
 COURANT_SQ = COURANT**2
+
+#material( 500 HZ) https://www.acoustic-supplies.com/absorption-coefficient-chart/
+brick_alpha = 0.03 # brick(natural)
+wooden_bench_alpha = 0.66 # Benches (wooden, fully occupied)
+
+SRC_X = N_2 // 2
+SRC_Y = N_2 // 2
+
+frame_duration = 0.02 # w sekundach
+delay_gauss = 4 * SIGMA
 
 def print_config():
     print("-" * 30)
--- src/main.py
+++ src/main.py
@@ -1,68 +1,158 @@
 import taichi as ti
-from config import N,COURANT_SQ, DT, FREQ_MAX
+from config import N,COURANT_SQ, DT, FREQ_MAX, N_2, brick_alpha, wooden_bench_alpha, COURANT, SIGMA, AMPLITUDE, SRC_X, SRC_Y, DT, frame_duration, delay_gauss
 import math
 import numpy as np
+import time
 
 ti.init(arch=ti.gpu)
 
-p_old = ti.field(ti.f32, shape=(N, N))
-p_curr = ti.field(ti.f32, shape=(N, N))
+p_old = ti.field(ti.f32, shape=(N_2, N_2))
+p_curr = ti.field(ti.f32, shape=(N_2, N_2))
 
-mask = ti.field(dtype=ti.i32, shape=(N, N)) # gdzie jest sciana
-
-@ti.kernel
-def setup_mask():
-    for i, j in mask:
-        if i <= 1 or i >= N - 2 or j <= 1 or j >= N - 2:
-            mask[i, j] = 1
-        if N // 4 < i < N // 2 and N // 3 < j < N // 3 + 10:
-            mask[i, j] = 1
+k_field = ti.field(dtype=ti.i32, shape=(N_2, N_2)) #ilosc sasiadow
+alpha_field = ti.field(dtype=ti.f32, shape=(N_2, N_2)) # współczynniki pochlaniania
+bk_field = ti.field(dtype=ti.f32, shape=(N_2, N_2)) # macierz bk
 
 @ti.kernel
-def step(p_prev: ti.template(), p_now: ti.template(), t: ti.f32):
+def generation_symulation_map():
+    for i,j in k_field: # domyslnie wszedzie 4 sąsiadów i otwarta przestrzen
+        k_field[i , j] = 4
+        alpha_field [i, j] = 1.0
+
+    for i,j in k_field: # warstwa zewnetrzna potrzebna do obliczeń
+        if i == 0 or j == 0 or i == N_2 - 1 or j == N_2 - 1:
+            k_field[i, j] = 0
+            alpha_field[i, j] = 0.0
+
+    for i,j in k_field: # sciany
+        if((i==1 and j ==1) or (i==1 and j==N_2-2) or (i==N_2-2 and j==1) or (i==N_2-2 and j==N_2-2)):
+            k_field[i, j] = 2
+            alpha_field[i, j] = brick_alpha
+
+        elif (i == 1 or i == N_2 - 2) and (j > 0 and j < N_2 -1):
+            k_field[i, j] = 3
+            alpha_field[i, j] = brick_alpha
+
+        elif (j == 1 or j == N_2 - 2) and (i > 0 and i < N_2 -1):
+            k_field[i, j] = 3
+            alpha_field[i, j] = brick_alpha
+
+    r_x1, r_x2 = 100, 200
+    r_y1, r_y2 = 100, 200
+
+    for i,j in k_field:
+        if i > r_x1 and i < r_x2 and j > r_y1 and j < r_y2:
+            k_field[i, j] = 0
+            alpha_field[i, j] = 0.0
+
+        elif (i == r_x1 and j == r_y1) or \
+                (i == r_x1 and j == r_y2) or \
+                (i == r_x2 and j == r_y1) or \
+                (i == r_x2 and j == r_y2):
+            k_field[i, j] = 4
+            alpha_field[i, j] = wooden_bench_alpha
+
+        elif (i == r_x1 or i == r_x2) and (j > r_y1 and j < r_y2):
+            k_field[i, j] = 3
+            alpha_field[i, j] = wooden_bench_alpha
+
+        elif (j == r_y1 or j == r_y2) and (i > r_x1 and i < r_x2):
+            k_field[i, j] = 3
+            alpha_field[i, j] = wooden_bench_alpha
+
+
+
+
+    for i,j in bk_field:
+        k_val = k_field[i, j]
+        beta_val = beta_from_alpha(alpha_field[i, j])
+
+        bk_field[i, j] = (4.0 - float(k_val)) * COURANT * beta_val / 2.0
+
+@ti.func
+def beta_from_alpha(alpha):
+    Beta = 0.0
+    if alpha >= 1.0:  # otwarta przestrzeń
+        Beta = 1.0
+    elif alpha <= 0.0:    # idealna ściana
+        Beta = 0.0
+    else:
+        R = ti.sqrt(1.0 - alpha)
+        Beta = (1.0 - R) / (1.0 + R)
+
+    return Beta
+
+
+
+@ti.kernel
+def step(p_prev: ti.template(), p_now: ti.template(), steps: int):
     for i,j in p_now:
-        if 0 < i < N - 1 and 0 < j < N - 1:
-            if mask[i, j] == 1:   #warunek Dirchleta
-                p_prev[i, j] = 0
-            else:
-                laplacian = p_now[i + 1, j] + p_now[i - 1, j] + \
-                            p_now[i, j + 1] + p_now[i, j - 1] - 4 * p_now[i, j]
-
-                p_prev[i, j] = COURANT_SQ * laplacian + 2 * p_now[i, j] - p_prev[i, j]
+        if k_field[i , j] > 0:
+            k_val = k_field[i,j]
+            bk_val = bk_field[i,j]
+            w1 =(2.0 - float(k_val) * COURANT_SQ) * p_now[i, j]
+            w2 = (bk_val - 1.0) * p_prev[i, j]
+            w3 = COURANT_SQ * (p_now[i + 1, j] + p_now[i - 1, j] + p_now[i, j + 1] + p_now[i, j - 1])
+            p_prev[i, j] = (w1 + w2 + w3) / (1 + bk_val)
+        else:
+            p_prev[i, j] = 0
 
     #ZRODLO
-    center = N // 2
-    if t < 0.05:
-        p_prev[center, center] += ti.sin(2.0 * math.pi * FREQ_MAX * t) #sygnal sinusoidalny
-        # jest soft source, += sprawia ze fala, która wróci, przejdzie po prostu przez głosnik
-
+    pulse = AMPLITUDE * ti.exp(- ((DT*steps - DT*delay_gauss)**2) / (2 * SIGMA**2))
+    p_prev[SRC_X, SRC_Y] += pulse # soft source
 
 
 def main():
-    gui = ti.GUI("2d - fdtd", res=(N, N))
-    setup_mask()
+    gui = ti.GUI("2d - fdtd", res=(N_2, N_2))
+    generation_symulation_map()
     buffers = [p_old, p_curr]
     print(f"Rozmiar siatki: {N}x{N}")
 
-    frame = 0
+    prev_time = time.time()
+
+    steps = 0
+    accumulator = 0
+
+    alpha_map = alpha_field.to_numpy()
+    is_wall = alpha_map < 0.99
+
     while gui.running:
-        b_old = frame % 2
-        b_curr = (frame + 1) % 2
 
-        step(buffers[b_old], buffers[b_curr], frame * DT)
+        now = time.time()
+        delta_real_time = now - prev_time
+        prev_time = now
 
-        p_np = buffers[b_old].to_numpy()
-        max_val = np.abs(p_np).max()
-        p_normalized = p_np / (max_val + 1e-8)
-        p_np_norm = p_normalized * 0.5 + 0.5
+        accumulator+=delta_real_time
+        if(accumulator >= frame_duration):
+            steps += 1
+            b_old = steps % 2
+            b_curr = (steps + 1) % 2
 
-        mask_np = mask.to_numpy() # zeby bylo widac sciany
+            step(buffers[b_old], buffers[b_curr], steps)
 
-        img = np.where(mask_np > 0, 1.0, p_np_norm)
-        gui.set_image(img)
+            p_np = buffers[b_old].to_numpy()
+            max_val = np.abs(p_np).max()
+            p_normalized = p_np / (max_val + 1e-8)
 
-        gui.show()
-        frame += 1
+            gray_val = p_normalized * 0.5 + 0.5
+
+            gray_val = np.clip(gray_val, 0.0, 1.0) # zabezpieczenie choc i tak juz wczesniej znormalizowane
+
+
+            img = np.dstack((gray_val, gray_val, gray_val))
+
+            img[is_wall, 1] *= 0.3
+            img[is_wall, 2] *= 0.2
+
+            gui.set_image(img)
+            gui.show()
+
+
+            accumulator = 0
+
+
+
+
 
 if __name__ == "__main__":
     main()
