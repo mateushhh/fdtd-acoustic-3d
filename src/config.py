a3bc5325880b274b9bfca68956bb8fa2bcbf456d
pomiano <miloszpomian@gmail.com>
2026-01-23 23:17:08 +0100

    poprawienie kodu

--- src/config.py
+++ src/config.py
@@ -5,36 +5,52 @@ def get_time_step(dimensions, dx, speed, safety_factor):
     courant_limit = 1.0 / math.sqrt(dimensions)
     return (dx / speed) * courant_limit * safety_factor
 
+# Parametry srodowiska
 C = 343.0   # predkosc [m/s]
-FREQ_MAX = 1000.0 # maksymalna czestotliwpsc fali [Hz]
 SIZE_M = 20.0  # rozmiar [m]
-SIGMA = math.sqrt(2 * math.log(2)) / (2 * math.pi * FREQ_MAX)
-AMPLITUDE = 10.0
-
-NODES_PER_WAVELENGTH = 10    # ilsoc punktow na dlugosc fali
-SAFETY_FACTOR = 0.99 # wspolczynnik bezpieczenstwa
 DIM = 2  # 2D
 
+# Parametry zrodla
+FREQ_MAX = 1000.0 # maksymalna czestotliwpsc fali [Hz]
+AMPLITUDE = 10.0
+
+#Parametry czasowe impulsu
+SIGMA = math.sqrt(2 * math.log(2)) / (2 * math.pi * FREQ_MAX) # sigma dla impulsu Gaussa
+DELAY_GAUSS = 4 * SIGMA # opóźnienie startu sygnału
+
+# Dyskretyzacja przestrzenna
+NODES_PER_WAVELENGTH = 10    # ilsoc punktow na dlugosc fali
 WAVELENGTH = C / FREQ_MAX    # dlugosc fali [m]
 DX = WAVELENGTH / NODES_PER_WAVELENGTH   # krok odleglosciowy [m]
 
+# Obliczanie liczby wezlow
 N = int(SIZE_M / DX)     # ilosc punktow w jednym wymiarze
 N_2 = N + 2 # dodajemy po jednym punckie na kazda strone do obliczen
 
-DT = get_time_step(DIM, DX, C, SAFETY_FACTOR)
-
-COURANT = (1.0 / math.sqrt(DIM)) * SAFETY_FACTOR
-COURANT_SQ = COURANT**2
-
-#material( 500 HZ) https://www.acoustic-supplies.com/absorption-coefficient-chart/
-brick_alpha = 0.03 # brick(natural)
-wooden_bench_alpha = 0.66 # Benches (wooden, fully occupied)
-
+# Polozenie zrodla
 SRC_X = N_2 // 2
 SRC_Y = N_2 // 2
 
-frame_duration = 0.02 # w sekundach
-delay_gauss = 4 * SIGMA
+# Wspolczynnik bezpieczenstwa
+SAFETY_FACTOR = 0.99
+
+# Warunek Couranta
+COURANT = (1.0 / math.sqrt(DIM)) * SAFETY_FACTOR
+COURANT_SQ = COURANT**2
+
+# Obliczanie kroku czasowego
+DT = get_time_step(DIM, DX, C, SAFETY_FACTOR)
+
+# wspolczynniki alfa dla materiałów przy fali o czestotliowsci 500 HZ
+# https://www.acoustic-supplies.com/absorption-coefficient-chart/
+MATERIALS = {
+    "brick": 0.03, # brick(natural)
+    "wooden_bench": 0.76 # Benches (wooden, fully occupied)
+}
+
+# Parametry animacji
+frame_duration = 0.02 # czas trwania jednej klatki wizualizacji [s]
+
 
 def print_config():
     print("-" * 30)
@@ -45,6 +61,3 @@ def print_config():
     print(f"Krok DT:         {DT*1e6:.2f} us")
     print(f"Liczba Couranta: {COURANT:.4f} (Max: {1/math.sqrt(DIM):.4f})")
     print("-" * 30)
-
-if __name__ == "__main__":
-    print_config()
--- src/main.py
+++ src/main.py
@@ -1,12 +1,11 @@
 import taichi as ti
-from config import N,COURANT_SQ, DT, FREQ_MAX, N_2, brick_alpha, wooden_bench_alpha, COURANT, SIGMA, AMPLITUDE, SRC_X, SRC_Y, DT, frame_duration, delay_gauss
-import math
+from config import N,COURANT_SQ, N_2, MATERIALS,COURANT, SIGMA, AMPLITUDE, SRC_X, SRC_Y, DT, frame_duration, DELAY_GAUSS, print_config
 import numpy as np
 import time
 
 ti.init(arch=ti.gpu)
 
-p_old = ti.field(ti.f32, shape=(N_2, N_2))
+p_old = ti.field(ti.f32, shape=(N_2, N_2)) # macierz ciśnień, do niej wsadzane są najnowsze wartosci
 p_curr = ti.field(ti.f32, shape=(N_2, N_2))
 
 k_field = ti.field(dtype=ti.i32, shape=(N_2, N_2)) #ilosc sasiadow
@@ -27,20 +26,21 @@ def generation_symulation_map():
     for i,j in k_field: # sciany
         if((i==1 and j ==1) or (i==1 and j==N_2-2) or (i==N_2-2 and j==1) or (i==N_2-2 and j==N_2-2)):
             k_field[i, j] = 2
-            alpha_field[i, j] = brick_alpha
+            alpha_field[i, j] = MATERIALS["brick"]
 
         elif (i == 1 or i == N_2 - 2) and (j > 0 and j < N_2 -1):
             k_field[i, j] = 3
-            alpha_field[i, j] = brick_alpha
+            alpha_field[i, j] = MATERIALS["brick"]
 
         elif (j == 1 or j == N_2 - 2) and (i > 0 and i < N_2 -1):
             k_field[i, j] = 3
-            alpha_field[i, j] = brick_alpha
+            alpha_field[i, j] = MATERIALS["brick"]
 
+    # wymiary przeszkód
     r_x1, r_x2 = 100, 200
     r_y1, r_y2 = 100, 200
 
-    for i,j in k_field:
+    for i,j in k_field:  # przeszkoda
         if i > r_x1 and i < r_x2 and j > r_y1 and j < r_y2:
             k_field[i, j] = 0
             alpha_field[i, j] = 0.0
@@ -50,18 +50,15 @@ def generation_symulation_map():
                 (i == r_x2 and j == r_y1) or \
                 (i == r_x2 and j == r_y2):
             k_field[i, j] = 4
-            alpha_field[i, j] = wooden_bench_alpha
+            alpha_field[i, j] = MATERIALS["wooden_bench"]
 
         elif (i == r_x1 or i == r_x2) and (j > r_y1 and j < r_y2):
             k_field[i, j] = 3
-            alpha_field[i, j] = wooden_bench_alpha
+            alpha_field[i, j] = MATERIALS["wooden_bench"]
 
         elif (j == r_y1 or j == r_y2) and (i > r_x1 and i < r_x2):
             k_field[i, j] = 3
-            alpha_field[i, j] = wooden_bench_alpha
-
-
-
+            alpha_field[i, j] = MATERIALS["wooden_bench"]
 
     for i,j in bk_field:
         k_val = k_field[i, j]
@@ -82,8 +79,6 @@ def beta_from_alpha(alpha):
 
     return Beta
 
-
-
 @ti.kernel
 def step(p_prev: ti.template(), p_now: ti.template(), steps: int):
     for i,j in p_now:
@@ -98,15 +93,15 @@ def step(p_prev: ti.template(), p_now: ti.template(), steps: int):
             p_prev[i, j] = 0
 
     #ZRODLO
-    pulse = AMPLITUDE * ti.exp(- ((DT*steps - DT*delay_gauss)**2) / (2 * SIGMA**2))
+    pulse = AMPLITUDE * ti.exp(- ((DT*steps - DT*DELAY_GAUSS)**2) / (2 * SIGMA**2))
     p_prev[SRC_X, SRC_Y] += pulse # soft source
 
 
 def main():
     gui = ti.GUI("2d - fdtd", res=(N_2, N_2))
+    print_config()
     generation_symulation_map()
     buffers = [p_old, p_curr]
-    print(f"Rozmiar siatki: {N}x{N}")
 
     prev_time = time.time()
 
@@ -114,7 +109,7 @@ def main():
     accumulator = 0
 
     alpha_map = alpha_field.to_numpy()
-    is_wall = alpha_map < 0.99
+    is_wall = alpha_map < 1.0
 
     is_paused = False
     while gui.running:
@@ -136,25 +131,18 @@ def main():
 
             p_np = buffers[b_old].to_numpy()
             max_val = np.abs(p_np).max()
-            p_normalized = p_np / (max_val + 1e-8)
-
-            gray_val = p_normalized * 0.5 + 0.5
+            p_normalized = p_np / (max_val + 1e-8) # normalizujemy poniewaz chcemy uzyskac wartosci od -1 do 1
+            gray_val = p_normalized * 0.5 + 0.5 # uzyskujemy wartoci od 0 do 1
 
             gray_val = np.clip(gray_val, 0.0, 1.0) # zabezpieczenie choc i tak juz wczesniej znormalizowane
-
-
             img = np.dstack((gray_val, gray_val, gray_val))
-
-            img[is_wall, 1] *= 0.3
-            img[is_wall, 2] *= 0.2
+            img[is_wall, 1] *= 0.3 # zielony na 30%
+            img[is_wall, 2] *= 0.3 # niebieski na 30%
 
             gui.set_image(img)
             gui.show()
 
             accumulator = 0
-
-
-
 
 
 if __name__ == "__main__":
